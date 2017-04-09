0672ece8f88f18fad6bd915d18f71ce5e00f5500
Jeff Willener <Jeff.Willener@mbshome.com>
2017-04-09 17:39:07 -0600

    Added missing ObjectExtension.

---
+++ OpenLogger/Extensions/ObjectExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OpenLogger.Extensions
+{
+    public static class ObjectExtensions
+    {
+        public static TResult IfNotNull<T, TResult>(this T value, Func<T, TResult> map)
+        {
+            if (map == null)
+                throw new ArgumentNullException("map");
+            return Equals(value, default(T))
+                       ? default(TResult)
+                       : map(value);
+        }
+    }
+}
--- OpenLogger/LogEventArgs.cs
+++ OpenLogger/LogEventArgs.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using OpenLogger.Extensions;
 
 namespace OpenLogger
 {
